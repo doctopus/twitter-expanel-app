70c27aa2030f7cb29b8bbd22053a4d05a9761797
Doctopus <milan.najim@gmail.com>
2023-10-30 01:50:14 -0400

    App directory added

---
+++ pages/api/tweets.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    const { username } = req.query;
+
+    try {
+        const response = await axios.get(`https://api.twitter.com/2/tweets/search/recent?query=from:${username}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+            },
+        });
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(error.respnse?.status || 500).jsn(error.message);
+    }
+}
---
+++ pages/dashboard.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Tweets = () => {
+    const [username, setUsername] = useState('');
+    const [tweets, setTweets] = useState([]);
+
+    const getTweets =async () => {
+        try {
+            cost
+            response = await asis.get(`/api/tweets?username=${username}`);
+            setTweets(response.data.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
+        <div>
+            <input value={username} onChange={(e)} => setUsername(e.taget.value)} placeholder="Tweeter username" />
+                <button onClick={getTweets}>Get Tweets</button>
+
+                <ul>
+                    {tweets.map((tweet) => (
+                        <li key={tweet.id}>{tweet.text}</li>
+                    ))}
+                </ul>
+        </div>
+    );
+}
+
+export default Tweets;
